cab3fcf4ca0615e1aae7fee38bb76382186bd86c
jishtesting <63588486+jishtesting@users.noreply.github.com>
2020-05-27 01:45:03 +0530

    Adding Zone and zone details

--- webhook.py
+++ webhook.py
@@ -10,16 +10,24 @@ import requests
 state_helpline = {"Andhra Pradesh": "0866-2410978","Arunachal Pradesh": "9436055743", "Assam" : "6913347770", "Bihar": "104","Chhattisgarh" :"104","Goa": "104","Gujarat" :"104", "Haryana" : "8558893911", "Himachal Pradesh" : "104", "Jharkhand" :"104", "Karnataka" :"104", "Kerala": "0471-2552056", "Madhya Pradesh" :"104",
 "Maharashtra" :"020-26127394", "Manipur" :"3852411668", "Meghalaya": "108", "Mizoram" :"102", "Nagaland": "7005539653", "Odisha" :"9439994859", "Punjab": "104", "Rajasthan" :"0141-2225624", "Sikkim" :"104", "Tamil Nadu": "044-29510500", "Telangana": "104", "Tripura" :"0381-2315879", "Uttarakhand" :"104","Uttar Pradesh": "18001805145",
 "West Bengal": "1800313444222, 03323412600","Andaman And Nicobar Islands": "03192-232102", "Chandigarh": "9779558282","Delhi" :"011-22307145", "Jammu & Kashmir" :"01912520982, 0194-2440283", "Ladakh" :"01982256462", "Lakshadweep" : "104", "Puducherry" :"104"}
+#
+# dict = {}
+# state = {}
+# counter = 0
+# list = []
 
 dict = {}
 state = {}
 counter = 0
-list = []
+list_cnfirm = []
+list_active = []
+list_decreased =[]
+list_recovered = []
 def sensor():
     global state, dict
     global counter
     global state_helpline
-    global list
+    global list_cnfirm, list_active, list_decreased, list_recovered
     """ Function for test purposes. """
     print("Scheduler is alive!")
     counter = counter + 1
@@ -30,14 +38,16 @@ def sensor():
     raw_data_helpline = requests.get("https://api.covid19india.org/resources/resources.json")
     jsondata_helpline = raw_data_helpline.json()
 
-    total = 0
+    raw_data = requests.get("https://api.covid19india.org/zones.json")
+    jsondata_zone = raw_data.json()
+
+    total_cnfirm = 0
+    total_active = 0
+    total_recovered = 0
+    total_decreased = 0
     for i in jsondata:
-        # print(i)
+
         dict.update(i)
-        # print(dict)
-        # print("***************************")
-        # print("state is " + str(dict["state"]))
-        # print("***************************")
 
         for j in dict["districtData"]:
             dist = j["district"]
@@ -49,31 +59,58 @@ def sensor():
 
             dict.update(district_data)
 
-            print(j["district"] + " total active cases " + str(j["active"]) + " total confirmed cases " + str(
-                j["confirmed"]) + " total deceased cases " + str(j["deceased"]) + " total recovered cases " + str(
-                j["recovered"]))
-            total = total + j["confirmed"]
-            state_data = {dict["state"]: str(total)}
+            total_cnfirm = total_cnfirm + j["confirmed"]
+            total_active = total_active + j["active"]
+            total_recovered = total_recovered + j["recovered"]
+            total_decreased = total_decreased + j["deceased"]
+            state_data = {dict["state"]: [str(total_cnfirm), str(total_active), str(total_recovered), str(total_decreased)]}
+                # 0 is confirm , 1 is active , 2 is recovered , 3 is decreased
             state.update(state_data)
-        print(dict["state"] + " Total " + str(total))
+        print(dict["state"] + " confirm " + str(total_cnfirm) + " active " + str(total_active) + " recovered " + str(
+            total_recovered) + " decreased " + str(total_decreased))
         print(state)
-        list.append(int(state_data.get(dict["state"])))
-        print(list)
-        print(state_data.get(dict["state"]))
-        print(state)
-        total = 0
-    print(type(list[1]))
-    print("888888888888888888888888888888888888888")
-    indiancount = sum(list)
-    list = []
-    print(sum(list))
+
+        print(dict["state"])
+        print((state_data.get(dict["state"]))[0])
+        print((state_data.get(dict["state"]))[1])
+        print((state_data.get(dict["state"]))[2])
+        print((state_data.get(dict["state"]))[3])
+
+        #     print(state)
+        list_cnfirm.append(int((state_data.get(dict["state"]))[0]))
+        list_active.append(int((state_data.get(dict["state"]))[1]))
+        list_recovered.append(int((state_data.get(dict["state"]))[2]))
+        list_decreased.append(int((state_data.get(dict["state"]))[3]))
+
+        print(list_cnfirm)
+        print(list_active)
+        print(list_recovered)
+        print(list_decreased)
+
+        total_cnfirm = 0
+        total_active = 0
+        total_decreased = 0
+        total_recovered = 0
+
+    indian_confirm = sum(list_cnfirm)
+    list_cnfirm = []
+    indian_active = sum(list_active)
+    list_active = []
+    indian_recovered = sum(list_recovered)
+    list_recovered = []
+    indian_decreased = sum(list_decreased)
+    list_decreased = []
+    print(indian_confirm)
+    print(indian_active)
+    print(indian_recovered)
+    print(indian_decreased)
 
 
     sched = BackgroundScheduler(daemon=True)
     sched.add_job(sensor,'interval',minutes=120)
     sched.start()
 
-    return dict, state, jsondata_helpline, indiancount
+    return dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased
 
 
 app = Flask(__name__)
@@ -97,7 +134,7 @@ def webhook():
 
 def process_request(req):
     global dict, state
-    global indiancount
+    global indian_confirm, indian_active, indian_recovered, indian_decreased
 
 
     try:
@@ -111,7 +148,7 @@ def process_request(req):
             print("Webhook Successfully connected.")
 
         elif action == "district":
-            dict, state,jsondata_helpline, indiancount = sensor()
+            dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased = sensor()
             district_name = req.get("queryResult").get("parameters").get("districtname")
             print(district_name)
             if district_name in dict:
@@ -166,14 +203,76 @@ def process_request(req):
                     ],
                 }
             return message
+        elif action == "zone":
+            dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased = sensor()
+            districtname = req.get("queryResult").get("parameters").get("districtname")
+            print(districtname)
+
+            for i in jsondata_zone["zones"]:
+                print(i)
+                if districtname in i["district"]:
+                    print(i["district"])
+                    print(i["zone"])
+                    return {
+                        "source": "webhook",
+                        "fulfillmentMessages": [
+                            {
+                                "text": {
+                                    "text":[
+                                        districtname + " is in " + i["zone"] + " Zone"
+                                    ]
+                                },
+                                "platform": "TELEGRAM"
+                            },
+                            {
+                                "quickReplies": {
+                                    "title": "What would you like to do next?",
+                                    "quickReplies": [
+                                        "Goverment Helpline No",
+                                        "Guidelines for "+ i["zone"] + " Zone",
+                                        "District wise Count",
+                                        "Helpful websites",
+
+                                    ]
+                                },
+                                "platform": "TELEGRAM"
+                            }
+                        ]
+                    }
+                # else:
+                #     message = {
+                #         "source": "webhook",
+                #         "fulfillmentMessages": [
+                #             {
+                #                 "text": {
+                #                     "text": [
+                #                         "Please enter the District name correctly"
+                #                     ]
+                #                 },
+                #                 "platform": "TELEGRAM"
+                #             },
+                #             {
+                #                 "quickReplies": {
+                #                     "title": "What would you like to do next?",
+                #                     "quickReplies": [
+                #                         "Covid cases",
+                #                         "About covid"
+                #                     ]
+                #                 },
+                #                 "platform": "TELEGRAM"
+                #             }
+                #         ],
+                #     }
 
 
         elif action == "state":
-            dict, state, jsondata_helpline, indiancount = sensor()
+            dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased = sensor()
             statename = req.get("queryResult").get("parameters").get("statename")
             print(statename)
             if statename in state:
                 print(str(state[statename]))
+                # 0 is confirm , 1 is active , 2 is recovered , 3 is decreased
+
 
 
                 message = {
@@ -182,7 +281,7 @@ def process_request(req):
                         {
                             "text": {
                                 "text": [
-                                    "In "+statename+ " there are total " +str(state[statename])+ " cases"
+                                    "In "+statename+ " there are " +str(state[statename][1]) +" active cases " +str(state[statename][2]) +" recovered cases " +str(state[statename][3]) +" Deaths and "  +str(state[statename][0]) +" Total confirmed cases "
                                 ]
                             },
                             "platform": "TELEGRAM"
@@ -231,15 +330,15 @@ def process_request(req):
 
 
         elif action == "india":
-            dict, state, jsondata_helpline, indiancount = sensor()
-            print(indiancount)
+            dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased = sensor()
+            # print(indiancount)
             return {
                 "source": "webhook",
                 "fulfillmentMessages": [
                     {
                         "text": {
                             "text": [
-                                "In india there are around " +str(indiancount)+ " cases"
+                                "In india there are around " +str(indian_active)+ " Active cases, " +str(indian_recovered)+ " Recovered cases, " +str(indian_decreased)+ " Decreased cases, " +str(indian_confirm)+ " and Confirm cases "
                             ]
                         },
                         "platform": "TELEGRAM"
@@ -269,7 +368,7 @@ def process_request(req):
             statename = req.get("queryResult").get("parameters").get("statename")
             print(statename)
             districtname = req.get("queryResult").get("parameters").get("districtname")
-            dict, state, jsondata_helpline, indiancount = sensor()
+            dict, state, jsondata_helpline, jsondata_zone, indian_confirm, indian_active, indian_recovered, indian_decreased = sensor()
             if statename :
                 statename.capitalize()
 
